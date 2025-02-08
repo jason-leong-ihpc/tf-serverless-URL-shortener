0483c053a876ab163575dd81f36588bec47d7493
yunfengsg <yunfengsg@gmail.com>
2025-02-08 12:37:52 +0800

    yyf 1st update

--- DynamoDB.tf
+++ DynamoDB.tf
@@ -1,14 +1,12 @@
-provider "aws" {
-  region  = "ap-southeast-1a"
-  version = "~> 2.0"
-}
 
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "UrLShortener-DynamoDB"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "Name"
+resource "aws_dynamodb_table" "table" {
+  name         = "${local.name_prefix}-UrLShortener-DynamoDBs"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "year"
+
+  attribute {
+    name = "year"
+    type = "" # todo: fill with apporpriate value
+  }
 
 }
