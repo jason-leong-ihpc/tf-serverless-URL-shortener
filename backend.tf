a1329cee47ce5d680dc3ee0d5e776a8102330225
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2025-02-09 09:12:23 +0800

    add version

--- backend.tf
+++ backend.tf
@@ -1,4 +1,19 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.50.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.2.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+
+terraform {
   backend "s3" {
     bucket  = "sctp-ce8-tfstate"                            # Use the bucket name from your create-s3-bucket setup
     key     = "terraform/ce8-coaching-16/terraform.tfstate" # Unique key for your state file
